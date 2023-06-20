d47f86e73dea6c353da898fe22f64a75b1333577
cmax396 <109488523+caden-maxwell@users.noreply.github.com>
2023-06-20 14:03:47 -0600

    Disable/enable log window buttons

--- src/app/pages/logs_window.py
+++ src/app/pages/logs_window.py
@@ -25,12 +25,17 @@ class LogsWindow(QWidget):
 
     def handle_logger_message(self, msg):
         self.ui.logsEdit.append(msg)
+        self.ui.saveBtn.setEnabled(True)
+        self.ui.clearBtn.setEnabled(True)
 
     def handle_clear_btn_clicked(self):
         self.ui.logsEdit.clear()
+        self.ui.clearBtn.setEnabled(False)
+        self.ui.saveBtn.setEnabled(False)
 
     def handle_save_btn_clicked(self):
         os.makedirs('logs', exist_ok=True)
         with open(f'logs/{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log', 'w') as f:
             f.write(self.ui.logsEdit.toPlainText())
         self.logger.info(f"Saved all logs to file at '{f.name}'.")
+        self.ui.saveBtn.setEnabled(False)
