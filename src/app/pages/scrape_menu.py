abb6dc8316d010759b39c321b7fbd87535718a13
cmax <a02335780@usu.edu>
2023-06-21 02:13:23 -0600

    Started main scrape engine. updated log book

--- src/app/pages/scrape_menu.py
+++ src/app/pages/scrape_menu.py
@@ -1,6 +1,5 @@
-import logging
 import json
-from time import sleep
+import logging
 
 from PyQt6.QtCore import pyqtSignal
 from PyQt6.QtWidgets import QWidget
@@ -8,7 +7,7 @@ from PyQt6.QtWidgets import QWidget
 from bs4 import BeautifulSoup
 
 from app.ui.ScrapeMenu_ui import Ui_ScrapeMenu
-from app.scrape.request_handler import ModelUpdateWorker, SearchRefreshWorker
+from app.scrape import ModelUpdateWorker, SearchRefreshWorker, ScrapeEngine
 
 from .data_view import DataView
 from .loading_window import LoadingWindow
@@ -24,13 +23,23 @@ class ScrapeMenu(QWidget):
         self.ui.setupUi(self)
 
         self.logger = logging.getLogger(__name__)
-        self.loading_window = LoadingWindow()
+        self.scrape_engine = ScrapeEngine()
+        self.loading_window = LoadingWindow(self.scrape_engine)
 
-        self.populate_search()
+        self.get_dropdown_data()
 
         self.ui.backBtn.clicked.connect(self.back.emit)
         self.ui.submitBtn.clicked.connect(self.handle_submission)
+
         self.ui.makeCombo.currentIndexChanged.connect(self.handle_make_change)
+        self.ui.modelCombo.currentIndexChanged.connect(lambda: self.scrape_engine.set_param("model", self.ui.modelCombo.currentText()))
+        self.ui.startYearCombo.currentIndexChanged.connect(lambda: self.scrape_engine.set_param("start_year", self.ui.startYearCombo.currentText()))
+        self.ui.endYearCombo.currentIndexChanged.connect(lambda: self.scrape_engine.set_param("end_year", self.ui.endYearCombo.currentText()))
+        self.ui.pDmgCombo.currentIndexChanged.connect(lambda: self.scrape_engine.set_param("primary_dmg", self.ui.pDmgCombo.currentText()))
+        self.ui.sDmgCombo.currentIndexChanged.connect(lambda: self.scrape_engine.set_param("secondary_dmg", self.ui.sDmgCombo.currentText()))
+        self.ui.minDvSpin.valueChanged.connect(lambda: self.scrape_engine.set_param("dv_min", self.ui.minDvSpin.value()))
+        self.ui.maxDvSpin.valueChanged.connect(lambda: self.scrape_engine.set_param("dv_max", self.ui.maxDvSpin.value()))
+
         self.loading_window.view_btn_clicked.connect(self.open_data_viewer)
 
     def setup(self):
@@ -43,12 +52,12 @@ class ScrapeMenu(QWidget):
         self.ui.minDvSpin.setValue(0)
         self.ui.maxDvSpin.setValue(0)
 
-    def populate_search(self):
+    def get_dropdown_data(self):
         self.worker = SearchRefreshWorker()
-        self.worker.refreshed.connect(self.parse_refresh)
+        self.worker.retrieved.connect(self.parse_retrieved)
         self.worker.start()
 
-    def parse_refresh(self, response):
+    def parse_retrieved(self, response):
         soup = BeautifulSoup(response, 'html.parser')
         table = soup.find('table', id='searchTable')
         dropdowns = table.select('select')
@@ -60,20 +69,10 @@ class ScrapeMenu(QWidget):
         
         self.logger.debug(f"Dropdown data:\n{json.dumps(dropdown_data, indent=4)}")
 
-        # Clear the comboboxes
-        self.ui.makeCombo.clear()
-        self.ui.modelCombo.clear()
-        self.ui.startYearCombo.clear()
-        self.ui.endYearCombo.clear()
-        self.ui.pDmgCombo.clear()
-        self.ui.sDmgCombo.clear()
+        self.populate_combos(dropdown_data)
 
-        # Alphabetize model list
-        dropdown_data['ddlModel'].sort()
-        dropdown_data['ddlModel'].remove("All")
-        dropdown_data['ddlModel'].insert(0, "All")
+    def populate_combos(self, dropdown_data):
 
-        # Add the dropdown data to the appropriate comboboxes
         self.ui.makeCombo.addItems(dropdown_data['ddlMake'])
         self.ui.modelCombo.addItems(dropdown_data['ddlModel'])
         self.ui.startYearCombo.addItems(dropdown_data['ddlStartModelYear'])
@@ -81,9 +80,10 @@ class ScrapeMenu(QWidget):
         self.ui.pDmgCombo.addItems(dropdown_data['ddlPrimaryDamage'])
         self.ui.sDmgCombo.addItems(dropdown_data['lSecondaryDamage'])
         
-        self.logger.info("Search scrape finished. Populated search fields.")
+        self.logger.info("Search fields populated.")
 
     def handle_make_change(self, make):
+        self.scrape_engine.set_param("make", self.ui.makeCombo.itemText(make))
         make = self.ui.makeCombo.itemText(make)
         self.worker = ModelUpdateWorker(make)
         self.worker.updated.connect(self.update_model_combo)
@@ -101,8 +101,12 @@ class ScrapeMenu(QWidget):
         self.logger.info("Model update finished. Populated model field.")
 
     def handle_submission(self):
-        self.loading_window.show()
+        self.scrape_engine.start()
+        self.loading_window.exec()
 
     def open_data_viewer(self):
+        if self.scrape_engine.isRunning():
+            self.logger.info("Scrape engine is running. Terminating.")
+            self.scrape_engine.terminate()
         self.data_viewer = DataView(True)
         self.data_viewer.show()
---
+++ src/app/scrape/scraper.py
@@ -0,0 +1,55 @@
+import logging
+
+from PyQt6.QtCore import QThread
+
+
+class ScrapeEngine(QThread):
+    def __init__(self):
+        super().__init__()
+        self.logger = logging.getLogger(__name__)
+        self.start_year = "All"
+        self.end_year = "All"
+        self.make = "All"
+        self.model = "All"
+        self.dv_min = 0
+        self.dv_max = 0
+        self.primary_dmg = "All"
+        self.secondary_dmg = "All"
+        self.image_set = "All"
+
+    def set_param(self, param, value):
+        match param:
+            case "start_year": self.start_year = value
+            case "end_year": self.end_year = value
+            case "make": self.make = value
+            case "model": self.model = value
+            case "dv_min": self.dv_min = value
+            case "dv_max": self.dv_max = value
+            case "primary_dmg": self.primary_dmg = value
+            case "secondary_dmg": self.secondary_dmg = value
+            case "image_set": self.image_set = value
+            case _: self.logger.error(f"Invalid param: {param}"); return
+        self.logger.info(f"Set param: {param} to {value}")
+
+    def run(self):
+        self.logger.info("Scrape engine started.")
+        count = 0
+        
+        self.logger.debug( f"""Scrape engine params:
+{{
+    start_year: {self.start_year}
+    end_year: {self.end_year}
+    make: {self.make}
+    model: {self.model}
+    dv_min: {self.dv_min}
+    dv_max: {self.dv_max}
+    primary_dmg: {self.primary_dmg}
+    secondary_dmg: {self.secondary_dmg}
+    image_set: {self.image_set}
+}}"""
+        )
+
+        while count < 5:
+            self.logger.info(f"Scrape engine count: {count}")
+            count += 1
+            self.sleep(1)
