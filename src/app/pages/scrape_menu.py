879c8f687ca2672f88c368aec2c0904181f12328
cmax <a02335780@usu.edu>
2023-06-22 16:30:05 -0600

    Getting code ready so it can make query

--- src/app/pages/scrape_menu.py
+++ src/app/pages/scrape_menu.py
@@ -24,34 +24,40 @@ class ScrapeMenu(QWidget):
         self.logger = logging.getLogger(__name__)
 
         self.ui.backBtn.clicked.connect(self.back.emit)
-        self.ui.submitBtn.clicked.connect(self.handle_submission)
+        self.ui.submitBtn.clicked.connect(self.handle_submit)
 
         self.scrape_engine = ScrapeEngine()
         self.loading_window = LoadingWindow(self.scrape_engine)
         self.loading_window.view_btn_clicked.connect(self.open_data_viewer)
 
-        self.ui.makeCombo.currentTextChanged.connect(self.handle_make_change)
-        self.ui.makeCombo.currentTextChanged.connect(lambda: self.scrape_engine.set_param(self.ui.makeCombo.currentData()))
-        self.ui.modelCombo.currentTextChanged.connect(lambda: self.scrape_engine.set_param(self.ui.modelCombo.currentData()))
-        self.ui.startYearCombo.currentTextChanged.connect(lambda: self.scrape_engine.set_param(self.ui.startYearCombo.currentData()))
-        self.ui.endYearCombo.currentTextChanged.connect(lambda: self.scrape_engine.set_param(self.ui.endYearCombo.currentData()))
-        self.ui.pDmgCombo.currentTextChanged.connect(lambda: self.scrape_engine.set_param(self.ui.pDmgCombo.currentData()))
-        self.ui.sDmgCombo.currentTextChanged.connect(lambda: self.scrape_engine.set_param(self.ui.sDmgCombo.currentData()))
-        self.ui.dvMinSpin.valueChanged.connect(self.scrape_engine.set_param)
-        self.ui.dvMaxSpin.valueChanged.connect(self.scrape_engine.set_param)
+        self.ui.makeCombo.currentTextChanged.connect(self.fetch_models)
 
-        self.retrieve_dropdown_data()
+        # Set up signals to update the scrape engine payload when the user changes any search criteria
+        self.ui.makeCombo.currentTextChanged.connect(lambda: self.scrape_engine.update_payload('ddlMake', self.ui.makeCombo.currentData()))
+        self.ui.modelCombo.currentTextChanged.connect(lambda: self.scrape_engine.update_payload('ddlModel', self.ui.modelCombo.currentData()))
+        self.ui.startYearCombo.currentTextChanged.connect(lambda: self.scrape_engine.update_payload('ddlStartModelYear', self.ui.startYearCombo.currentData()))
+        self.ui.endYearCombo.currentTextChanged.connect(lambda: self.scrape_engine.update_payload('ddlEndModelYear', self.ui.endYearCombo.currentData()))
+        self.ui.pDmgCombo.currentTextChanged.connect(lambda: self.scrape_engine.update_payload('ddlPrimaryDamage', self.ui.pDmgCombo.currentData()))
+        self.ui.sDmgCombo.currentTextChanged.connect(lambda: self.scrape_engine.update_payload('lSecondaryDamage', self.ui.sDmgCombo.currentData()))
+        self.ui.dvMinSpin.valueChanged.connect(lambda: self.scrape_engine.update_payload('tDeltaVFrom', self.ui.dvMinSpin.value()))
+        self.ui.dvMaxSpin.valueChanged.connect(lambda: self.scrape_engine.update_payload('tDeltaVTo', self.ui.dvMaxSpin.value()))
+
+        self.fetch_search()
 
     def showEvent(self, event):
-        self.refresh_worker.start()
+        self.fetch_search()
         return super().showEvent(event)
 
-    def retrieve_dropdown_data(self):
+    def fetch_search(self):
+        """Fetches the NASS/CDS search website and calls parse_retrieved once there is a response."""
         self.refresh_worker = SearchRefreshWorker()
-        self.refresh_worker.retrieved.connect(self.parse_retrieved)
+        self.refresh_worker.retrieved.connect(self.parse_search)
         self.refresh_worker.start()
 
-    def parse_retrieved(self, response):
+    def parse_search(self, response):
+        """Parses the response from the NASS/CDS search website and populates the search fields."""
+
+        # Parse response
         soup = BeautifulSoup(response, 'html.parser')
         table = soup.find('table', id='searchTable')
         dropdowns = table.select('select')
@@ -61,16 +67,13 @@ class ScrapeMenu(QWidget):
             options = dropdown.find_all('option')
             dropdown_data[dropdown['name']] = [(option.text,option.get('value')) for option in options]
         
-        self.populate_combos(dropdown_data)
-
-    def populate_combos(self, dropdown_data):
-
-        # Disconnect signal temporarily to prevent unnessesary calls to handle_make_change
-        self.ui.makeCombo.currentTextChanged.disconnect(self.handle_make_change)
+        # Block signals temporarily to prevent unnessesary call to handle_make_change while populating make dropdown
+        self.ui.makeCombo.blockSignals(True)
         for data in dropdown_data['ddlMake']:
             self.ui.makeCombo.addItem(*data)
-        self.ui.makeCombo.currentTextChanged.connect(self.handle_make_change)
+        self.ui.makeCombo.blockSignals(False)
 
+        # Populate remaining dropdowns
         for data in dropdown_data['ddlModel']:
             self.ui.modelCombo.addItem(*data)
 
@@ -88,30 +91,41 @@ class ScrapeMenu(QWidget):
         
         self.logger.info("Search fields populated.")
 
-    def handle_make_change(self, make):
+    def fetch_models(self, make):
+        """Fetches the models for the given make and calls update_model_dropdown once there is a response."""
         self.update_worker = ModelUpdateWorker(make)
-        self.update_worker.updated.connect(self.update_model_combo)
+        self.update_worker.updated.connect(self.update_model_dropdown)
         self.update_worker.start()
 
-    def update_model_combo(self, response):
+    def update_model_dropdown(self, response):
+        """Populates the model dropdown with models from the response."""
+
+        # Parse response
         model_dcts = json.loads(response)
         models = []
         for model in model_dcts:
             models.append((model['Value'], model['Key']))
         models.sort()
-        self.ui.modelCombo.disconnect()
+
+        # Block signals temporarily to prevent unnessesary call to update_model while clearing dropdown
+        self.ui.modelCombo.blockSignals(True)
         self.ui.modelCombo.clear()
-        self.ui.modelCombo.currentTextChanged.connect(lambda s: self.scrape_engine.set_param("model", s))
+        self.ui.modelCombo.blockSignals(False)
+
+        # Populate model dropdown
         self.ui.modelCombo.addItem("All", -1)
         for model in models:
             self.ui.modelCombo.addItem(*model)
         self.logger.info("Updated model dropdown.")
 
-    def handle_submission(self):
+
+    def handle_submit(self):
+        """Starts the scrape engine with the given parameters."""
         self.scrape_engine.start()
         self.loading_window.exec()
 
     def open_data_viewer(self):
+        """Opens the data viewer window, terminating the scrape engine if it is running."""
         if self.scrape_engine.isRunning():
             self.logger.info("Scrape engine is running. Terminating.")
             self.scrape_engine.terminate()
--- src/app/scrape/scraper.py
+++ src/app/scrape/scraper.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 from PyQt6.QtCore import QThread
@@ -7,45 +8,32 @@ class ScrapeEngine(QThread):
     def __init__(self):
         super().__init__()
         self.logger = logging.getLogger(__name__)
-        self.make = "All"
-        self.model = "All"
-        self.start_year = "All"
-        self.end_year = "All"
-        self.dv_min = 0
-        self.dv_max = 0
-        self.primary_dmg = "All"
-        self.secondary_dmg = "All"
         self.image_set = "All"
 
-    def set_param(self, value):
-        sender = self.sender()
-        match sender.objectName():
-            case "makeCombo": self.make = value
-            case "modelCombo": self.model = value
-            case "startYearCombo": self.start_year = value
-            case "endYearCombo": self.end_year = value
-            case "dvMinSpin": self.dv_min = value
-            case "dvMaxSpin": self.dv_max = value
-            case "pDmgCombo": self.primary_dmg = value
-            case "sDmgCombo": self.secondary_dmg = value
-            case "imageSetCombo": self.image_set = value
-            case _: self.logger.error(f"Invalid sender: {sender.objectName()}"); return
-        self.logger.debug(f"Set {sender.objectName()} to {value}")
+        # Get deafult search payload
+        with open("app/scrape/payload.json", "r") as f:
+            self.search_payload = json.load(f)
+
+    def update_payload(self, key, val):
+        self.search_payload[key] = val
+        self.logger.info(f"{key} updated to '{val}'.")
 
     def run(self):
         self.logger.debug( f"""Scrape engine started with these params:
 {{
-    make: {self.make},
-    model: {self.model},
-    model_start_year: {self.start_year},
-    model_end_year: {self.end_year},
-    dv_min: {self.dv_min},
-    dv_max: {self.dv_max},
-    primary_dmg: {self.primary_dmg},
-    secondary_dmg: {self.secondary_dmg},
+    make: {self.search_payload['ddlMake']},
+    model: {self.search_payload['ddlModel']},
+    model_start_year: {self.search_payload['ddlStartModelYear']},
+    model_end_year: {self.search_payload['ddlEndModelYear']},
+    dv_min: {self.search_payload['tDeltaVFrom']},
+    dv_max: {self.search_payload['tDeltaVTo']},
+    primary_dmg: {self.search_payload['ddlPrimaryDamage']},
+    secondary_dmg: {self.search_payload['lSecondaryDamage']},
     image_set: {self.image_set}
 }}"""
         )
+
+        
 
 
         self.logger.info("Scrape engine finished.")
