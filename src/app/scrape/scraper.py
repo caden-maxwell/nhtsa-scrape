e95f3bba7efa3b64228b6bb8b8423e9ce443265b
cmax <a02335780@usu.edu>
2023-06-21 18:29:01 -0600

    Minor class changes

--- src/app/scrape/scraper.py
+++ src/app/scrape/scraper.py
@@ -7,11 +7,11 @@ class ScrapeEngine(QThread):
     def __init__(self):
         super().__init__()
         self.logger = logging.getLogger(__name__)
-        self.model_start_year = "All"
-        self.model_end_year = "All"
         self.make = "All"
         self.model = "All"
-        self.dv_max = 0
+        self.start_year = "All"
+        self.end_year = "All"
+        self.dv_min = 0
         self.dv_max = 0
         self.primary_dmg = "All"
         self.secondary_dmg = "All"
@@ -20,10 +20,10 @@ class ScrapeEngine(QThread):
     def set_param(self, value):
         sender = self.sender()
         match sender.objectName():
-            case "startYearCombo": self.model_start_year = value
-            case "endYearCombo": self.model_end_year = value
             case "makeCombo": self.make = value
             case "modelCombo": self.model = value
+            case "startYearCombo": self.start_year = value
+            case "endYearCombo": self.end_year = value
             case "dvMinSpin": self.dv_min = value
             case "dvMaxSpin": self.dv_max = value
             case "pDmgCombo": self.primary_dmg = value
@@ -35,14 +35,14 @@ class ScrapeEngine(QThread):
     def run(self):
         self.logger.debug( f"""Scrape engine started with these params:
 {{
-    start_year: {self.start_year}
-    end_year: {self.end_year}
-    make: {self.make}
-    model: {self.model}
-    dv_min: {self.dv_min}
-    dv_max: {self.dv_max}
-    primary_dmg: {self.primary_dmg}
-    secondary_dmg: {self.secondary_dmg}
+    make: {self.make},
+    model: {self.model},
+    model_start_year: {self.start_year},
+    model_end_year: {self.end_year},
+    dv_min: {self.dv_min},
+    dv_max: {self.dv_max},
+    primary_dmg: {self.primary_dmg},
+    secondary_dmg: {self.secondary_dmg},
     image_set: {self.image_set}
 }}"""
         )
