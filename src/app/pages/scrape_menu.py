2c2d5b8a8abd1a13d310eb83589624fecf7b8994
cmax396 <109488523+caden-maxwell@users.noreply.github.com>
2023-06-21 18:26:02 -0600

    startYearCombo

--- src/app/pages/scrape_menu.py
+++ src/app/pages/scrape_menu.py
@@ -21,36 +21,28 @@ class ScrapeMenu(QWidget):
 
         self.ui = Ui_ScrapeMenu()
         self.ui.setupUi(self)
-
         self.logger = logging.getLogger(__name__)
-        self.scrape_engine = ScrapeEngine()
-        self.loading_window = LoadingWindow(self.scrape_engine)
-
-        self.retrieve_dropdown_data()
 
         self.ui.backBtn.clicked.connect(self.back.emit)
         self.ui.submitBtn.clicked.connect(self.handle_submission)
 
-        self.ui.makeCombo.currentIndexChanged.connect(self.handle_make_change)
-        self.ui.modelCombo.currentIndexChanged.connect(lambda: self.scrape_engine.set_param("model", self.ui.modelCombo.currentText()))
-        self.ui.startYearCombo.currentIndexChanged.connect(lambda: self.scrape_engine.set_param("start_year", self.ui.startYearCombo.currentText()))
-        self.ui.endYearCombo.currentIndexChanged.connect(lambda: self.scrape_engine.set_param("end_year", self.ui.endYearCombo.currentText()))
-        self.ui.pDmgCombo.currentIndexChanged.connect(lambda: self.scrape_engine.set_param("primary_dmg", self.ui.pDmgCombo.currentText()))
-        self.ui.sDmgCombo.currentIndexChanged.connect(lambda: self.scrape_engine.set_param("secondary_dmg", self.ui.sDmgCombo.currentText()))
-        self.ui.minDvSpin.valueChanged.connect(lambda: self.scrape_engine.set_param("dv_min", self.ui.minDvSpin.value()))
-        self.ui.maxDvSpin.valueChanged.connect(lambda: self.scrape_engine.set_param("dv_max", self.ui.maxDvSpin.value()))
-
+        self.scrape_engine = ScrapeEngine()
+        self.loading_window = LoadingWindow(self.scrape_engine)
         self.loading_window.view_btn_clicked.connect(self.open_data_viewer)
 
+        self.ui.makeCombo.currentTextChanged.connect(self.handle_make_change)
+        self.ui.makeCombo.currentTextChanged.connect(lambda: self.scrape_engine.set_param(self.ui.makeCombo.currentData()))
+        self.ui.modelCombo.currentTextChanged.connect(lambda: self.scrape_engine.set_param(self.ui.modelCombo.currentData()))
+        self.ui.startYearCombo.currentTextChanged.connect(lambda: self.scrape_engine.set_param(self.ui.startYearCombo.currentData()))
+        self.ui.endYearCombo.currentTextChanged.connect(lambda: self.scrape_engine.set_param(self.ui.endYearCombo.currentData()))
+        self.ui.pDmgCombo.currentTextChanged.connect(lambda: self.scrape_engine.set_param(self.ui.pDmgCombo.currentData()))
+        self.ui.sDmgCombo.currentTextChanged.connect(lambda: self.scrape_engine.set_param(self.ui.sDmgCombo.currentData()))
+        self.ui.dvMinSpin.valueChanged.connect(self.scrape_engine.set_param)
+        self.ui.dvMaxSpin.valueChanged.connect(self.scrape_engine.set_param)
+
+        self.retrieve_dropdown_data()
+
     def showEvent(self, event):
-        self.ui.makeCombo.setCurrentText("All")
-        self.ui.modelCombo.setCurrentText("All")
-        self.ui.startYearCombo.setCurrentText("All")
-        self.ui.endYearCombo.setCurrentText("All")
-        self.ui.pDmgCombo.setCurrentText("All")
-        self.ui.sDmgCombo.setCurrentText("All")
-        self.ui.minDvSpin.setValue(0)
-        self.ui.maxDvSpin.setValue(0)
         self.refresh_worker.start()
         return super().showEvent(event)
 
@@ -67,28 +59,36 @@ class ScrapeMenu(QWidget):
         dropdown_data = {}
         for dropdown in dropdowns:
             options = dropdown.find_all('option')
-            dropdown_data[dropdown['name']] = [option.text for option in options]
+            dropdown_data[dropdown['name']] = [(option.text,option.get('value')) for option in options]
         
         self.populate_combos(dropdown_data)
 
     def populate_combos(self, dropdown_data):
 
         # Disconnect signal temporarily to prevent unnessesary calls to handle_make_change
-        self.ui.makeCombo.disconnect()
-        self.ui.makeCombo.addItems(dropdown_data['ddlMake'])
-        self.ui.makeCombo.currentIndexChanged.connect(self.handle_make_change)
+        self.ui.makeCombo.currentTextChanged.disconnect(self.handle_make_change)
+        for data in dropdown_data['ddlMake']:
+            self.ui.makeCombo.addItem(*data)
+        self.ui.makeCombo.currentTextChanged.connect(self.handle_make_change)
 
-        self.ui.modelCombo.addItems(dropdown_data['ddlModel'])
-        self.ui.startYearCombo.addItems(dropdown_data['ddlStartModelYear'])
-        self.ui.endYearCombo.addItems(dropdown_data['ddlEndModelYear'])
-        self.ui.pDmgCombo.addItems(dropdown_data['ddlPrimaryDamage'])
-        self.ui.sDmgCombo.addItems(dropdown_data['lSecondaryDamage'])
+        for data in dropdown_data['ddlModel']:
+            self.ui.modelCombo.addItem(*data)
+
+        for data in dropdown_data['ddlStartModelYear']:
+            self.ui.startYearCombo.addItem(*data)
+        
+        for data in dropdown_data['ddlEndModelYear']:
+            self.ui.endYearCombo.addItem(*data)
+
+        for data in dropdown_data['ddlPrimaryDamage']:
+            self.ui.pDmgCombo.addItem(*data)
+            
+        for data in dropdown_data['lSecondaryDamage']:
+            self.ui.sDmgCombo.addItem(*data)
         
         self.logger.info("Search fields populated.")
 
     def handle_make_change(self, make):
-        self.scrape_engine.set_param("make", self.ui.makeCombo.itemText(make))
-        make = self.ui.makeCombo.itemText(make)
         self.update_worker = ModelUpdateWorker(make)
         self.update_worker.updated.connect(self.update_model_combo)
         self.update_worker.start()
@@ -97,11 +97,14 @@ class ScrapeMenu(QWidget):
         model_dcts = json.loads(response)
         models = []
         for model in model_dcts:
-            models.append(model['Value'])
+            models.append((model['Value'], model['Key']))
         models.sort()
+        self.ui.modelCombo.disconnect()
         self.ui.modelCombo.clear()
-        self.ui.modelCombo.addItem("All")
-        self.ui.modelCombo.addItems(models)
+        self.ui.modelCombo.currentTextChanged.connect(lambda s: self.scrape_engine.set_param("model", s))
+        self.ui.modelCombo.addItem("All", -1)
+        for model in models:
+            self.ui.modelCombo.addItem(*model)
         self.logger.info("Updated model dropdown.")
 
     def handle_submission(self):
