7a450bbf0a6dccdea0d76e95f5a4246829e8b418
cmax <a02335780@usu.edu>
2023-06-26 16:12:18 -0600

    Forgot to change this too

--- src/app/pages/logs_window.py
+++ src/app/pages/logs_window.py
@@ -1,7 +1,7 @@
 import datetime
 import logging
 import os
-from PyQt6 import QtGui
+from pathlib import Path
 
 from PyQt6.QtCore import pyqtSignal
 from PyQt6.QtWidgets import QWidget
@@ -21,8 +21,8 @@ class LogsWindow(QWidget):
 
         self.logger = logging.getLogger(__name__)
 
-        self.ui.clearBtn.clicked.connect(self.handle_clear_btn_clicked)
-        self.ui.saveBtn.clicked.connect(self.handle_save_btn_clicked)
+        self.ui.clearBtn.clicked.connect(self.handle_clear_clicked)
+        self.ui.saveBtn.clicked.connect(self.handle_save_clicked)
 
     def showEvent(self, a0):
         self.ui.logsEdit.verticalScrollBar().setValue(self.ui.logsEdit.verticalScrollBar().maximum())
@@ -33,14 +33,16 @@ class LogsWindow(QWidget):
         self.ui.saveBtn.setEnabled(True)
         self.ui.clearBtn.setEnabled(True)
 
-    def handle_clear_btn_clicked(self):
+    def handle_clear_clicked(self):
         self.ui.logsEdit.clear()
         self.ui.clearBtn.setEnabled(False)
         self.ui.saveBtn.setEnabled(False)
 
-    def handle_save_btn_clicked(self):
-        os.makedirs('logs', exist_ok=True)
-        with open(f'logs/{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log', 'w') as f:
+    def handle_save_clicked(self):
+        dir_path = Path(__file__).parent.parent / "logs"
+        os.makedirs(dir_path, exist_ok=True)
+        log_path = dir_path / f'{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log'
+        with open(log_path, 'w') as f:
             f.write(self.ui.logsEdit.toPlainText())
         self.logger.info(f"Saved all logs to file at '{f.name}'.")
         self.ui.saveBtn.setEnabled(False)
