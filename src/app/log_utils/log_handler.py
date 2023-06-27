ff80d33b3ead4a61b1b8a29d82e130a39ac176a1
cmax396 <109488523+caden-maxwell@users.noreply.github.com>
2023-06-27 12:16:13 -0600

    Simple color formatter for logs

--- src/app/log_utils/log_handler.py
+++ src/app/log_utils/log_handler.py
@@ -1,5 +1,7 @@
-from PyQt6.QtCore import QObject, pyqtSignal
 import logging
+
+from PyQt6.QtCore import QObject, pyqtSignal
+
 
 class QtLogHandler(logging.Handler, QObject):
     log_message = pyqtSignal(str)
