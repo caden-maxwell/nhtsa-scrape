dd92b403083a3f3ffd187c3f35e5b3b4cd0e1dd2
cmax <a02335780@usu.edu>
2023-07-16 03:21:07 -0600

    Event page layout changed to make things easier

--- src/app/pages/events_tab.py
+++ src/app/pages/events_tab.py
@@ -5,15 +5,7 @@ from pathlib import Path
 
 from PyQt6.QtCore import Qt, pyqtSlot
 from PyQt6.QtGui import QPixmap, QFont, QImage
-from PyQt6.QtWidgets import (
-    QWidget,
-    QLabel,
-    QGridLayout,
-    QHBoxLayout,
-    QScrollArea,
-    QLabel,
-    QSizePolicy,
-)
+from PyQt6.QtWidgets import QWidget, QLabel, QLabel
 
 from bs4 import BeautifulSoup
 from PIL import Image
@@ -38,19 +30,23 @@ class EventsTab(QWidget):
 
         self.ui.eventsList.setModel(self.model)
         self.ui.eventsList.doubleClicked.connect(self.open_event_details)
+        self.ui.scrapeBtn.clicked.connect(self.scrape_images)
+        self.ui.discardBtn.clicked.connect(self.discard_event)
 
-        self.img_grid = ImageViewerWidget()
-        self.img_grid.setSizePolicy(
-            QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed
-        )
-        self.ui.gridLayout.addWidget(self.img_grid, 2, 0, 1, 2)
+        self.no_images_label = QLabel("There are no images to display")
+        font = QFont()
+        font.setPointSize(14)
+        self.no_images_label.setFont(font)
+        self.no_images_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.ui.imgWidgetGrid.addWidget(self.no_images_label, 0, 0)
+        self.no_images_label.setVisible(True)
 
         self.request_handler = RequestHandler()
         self.request_handler.response_received.connect(self.handle_response)
-        self.events_pending = []
-        self.response_cache = {}
 
-        self.previous_index = None
+        self.response_cache = {}
+        self.case_veh_img_ids = {}
+        self.vehicle_imgs = {}
 
     def showEvent(self, event) -> None:
         self.update_list_size()
@@ -77,98 +73,110 @@ class EventsTab(QWidget):
         }
 
     def open_event_details(self, index):
-        if self.ui.eventsList.currentIndex() == self.previous_index:
-            return
-        self.previous_index = index
-        self.img_grid.clear_images()
-        self.request_handler.clear_queue(Priority.IMAGE.value)
+        self.update_images()
 
-        for i in reversed(range(self.ui.eventLayout.count())):
-            self.ui.eventLayout.itemAt(i).widget().setParent(None)
+        for i in reversed(range(self.ui.eventLayoutLeft.count())):
+            self.ui.eventLayoutLeft.itemAt(i).widget().setParent(None)
+
+        for i in reversed(range(self.ui.eventLayoutRight.count())):
+            self.ui.eventLayoutRight.itemAt(i).widget().setParent(None)
 
         event_data = self.model.data(index, Qt.ItemDataRole.UserRole)
-        case_id = event_data["case_id"]
 
-        # If we have a cached response for this case, use it
-        if (
-            case_id in self.response_cache
-            and (datetime.now() - self.response_cache[case_id][1]).total_seconds()
-            < self.COOKIE_EXPIRED_SECS
-        ):
-            self.parse_case(
-                self.response_cache[case_id]["xml"], self.response_cache[case_id]["cookie"]
-            )
-            return
-
-        request = Request(
-            ScrapeEngine.CASE_URL + str(case_id), priority=Priority.CASE_FOR_IMAGE.value
+        keys_left_side = set(
+            ["make", "model", "model_year", "curb_weight", "dmg_loc", "underride"]
         )
-        self.request_handler.enqueue_request(request)
-        self.events_pending.append(
-            {"case_id": case_id, "vehicle_num": event_data["vehicle_num"]}
-        )
-        print(self.events_pending)
 
-        keys_to_keep = set(
-            [
-                "make",
-                "model",
-                "model_year",
-                "curb_weight",
-                "dmg_loc",
-                "underride",
-                "c_bar",
-                "NASS_dv",
-                "NASS_vc",
-                "TOT_dv",
-            ]
-        )
-        event_data = {k: v for k, v in event_data.items() if k in keys_to_keep}
-
-        for i, (key, value) in enumerate(event_data.items()):
+        left_col_data = {k: v for k, v in event_data.items() if k in keys_left_side}
+        for i, (key, value) in enumerate(left_col_data.items()):
             key_label = QLabel(str(key) + ":")
-            key_label.setWordWrap(True)
             key_label.setTextInteractionFlags(
                 Qt.TextInteractionFlag.TextSelectableByMouse
             )
             key_label.setAlignment(Qt.AlignmentFlag.AlignTop)
-            self.ui.eventLayout.addWidget(key_label, i + 1, 0)
+            font = key_label.font()
+            font.setWeight(QFont.Weight.Bold)
+            key_label.setFont(font)
+            self.ui.eventLayoutLeft.addWidget(key_label, i + 1, 0)
 
             value_label = QLabel(str(value))
-            value_label.setWordWrap(True)
             value_label.setTextInteractionFlags(
                 Qt.TextInteractionFlag.TextSelectableByMouse
             )
             value_label.setAlignment(Qt.AlignmentFlag.AlignTop)
-            self.ui.eventLayout.addWidget(value_label, i + 1, 1)
+            self.ui.eventLayoutLeft.addWidget(value_label, i + 1, 1)
 
-        self.ui.eventLayout.setColumnStretch(1, 1)
+        keys_right_side = set(["c_bar", "NASS_dv", "NASS_vc", "TOT_dv"])
+
+        event_data_right = {k: v for k, v in event_data.items() if k in keys_right_side}
+        for i, (key, value) in enumerate(event_data_right.items()):
+            key_label = QLabel(str(key) + ":")
+            key_label.setTextInteractionFlags(
+                Qt.TextInteractionFlag.TextSelectableByMouse
+            )
+            key_label.setAlignment(Qt.AlignmentFlag.AlignTop)
+            font = key_label.font()
+            font.setWeight(QFont.Weight.Bold)
+            key_label.setFont(font)
+            self.ui.eventLayoutRight.addWidget(key_label, i + 1, 0)
+
+            value_label = QLabel(f"{value:.2f}")
+            value_label.setTextInteractionFlags(
+                Qt.TextInteractionFlag.TextSelectableByMouse
+            )
+            value_label.setAlignment(Qt.AlignmentFlag.AlignTop)
+            self.ui.eventLayoutRight.addWidget(value_label, i + 1, 1)
+
+    def scrape_images(self):
+        event_data = self.model.data(self.ui.eventsList.currentIndex(), Qt.ItemDataRole.UserRole)
+        case_id = int(event_data["case_id"])
+        vehicle_num = int(event_data["vehicle_num"])
+
+        if not self.case_veh_img_ids.get((case_id, event_data["vehicle_num"])):
+            self.case_veh_img_ids[(case_id, vehicle_num)] = []
+
+        if (
+            case_id in self.response_cache
+            and (
+                datetime.now() - self.response_cache[case_id]["created"]
+            ).total_seconds()
+            < self.COOKIE_EXPIRED_SECS
+        ):
+            cached_case = self.response_cache[case_id]
+            self.parse_case(cached_case["xml"], cached_case["cookie"])
+        else:
+            request = Request(
+                ScrapeEngine.CASE_URL + str(case_id),
+                priority=Priority.CASE_FOR_IMAGE.value,
+            )
+            self.request_handler.enqueue_request(request)
+
+    def discard_event(self):
+        pass
 
     @pyqtSlot(int, str, bytes, str)
     def handle_response(self, priority, url, response_content, cookie):
         if priority == Priority.CASE_FOR_IMAGE.value:
             self.parse_case(response_content, cookie)
         elif priority == Priority.IMAGE.value:
-            self.parse_image(response_content)
+            self.parse_image(url, response_content)
 
     def parse_case(self, response_content, cookie):
         soup = BeautifulSoup(response_content, "xml")
-        case_id = soup.find("CaseForm").get("caseID")
+        case_id = int(soup.find("CaseForm").get("caseID"))
         img_form = soup.find("IMGForm")
+
+        self.cache_response(case_id, response_content, cookie)
 
         if not img_form:
             self.logger.debug("No ImgForm found.")
             return
 
-        vehicle_num = None
-        for pending in self.events_pending:
-            if pending["case_id"] == int(case_id):
-                vehicle_num = pending["vehicle_num"]
-                self.events_pending.remove(pending)
-                break
-        if not vehicle_num:
-            self.logger.warning("No matching pending event found.")
-            return
+        pending_veh_nums = [
+            key[1]
+            for key in self.case_veh_img_ids.keys()
+            if key[0] == case_id and not self.case_veh_img_ids[key]
+        ]
 
         veh_img_areas = {
             "F": "Front",
@@ -181,79 +189,62 @@ class EventsTab(QWidget):
             "BR": "Backrightoblique",
         }
 
-        img_set_lookup = {}
-        for k, v in veh_img_areas.items():
-            img_set_lookup[k] = [
-                (img.text, img["version"])
-                for img in img_form.find("Vehicle", {"VehicleNumber": {vehicle_num}})
-                .find(v)
-                .find_all("image")
-            ]
+        for vehicle_num in pending_veh_nums:
+            veh_img_form = img_form.find("Vehicle", {"VehicleNumber": {vehicle_num}})
+            if not veh_img_form:
+                self.logger.warning(
+                    f"Image form for vehicle '{vehicle_num}' of case '{case_id}' not found."
+                )
+                continue
 
-        image_set = "R"  # TODO: Get this from the UI. Default to right side for now.
+            img_set_lookup = {}
+            for key, val in veh_img_areas.items():
+                img_area_form = veh_img_form.find(val)
+                if not img_area_form:
+                    continue
+                images = img_area_form.find_all("image")
+                img_set_lookup[key] = [(img.text, img["version"]) for img in images]
 
-        img_elements = img_set_lookup.get(image_set, [])
-        for img_element in img_elements:
-            img_id = img_element[0]
-            img_version = img_element[1]
-            img_url = f"https://crashviewer.nhtsa.dot.gov/nass-cds/GetBinary.aspx?Image&ImageID={img_id}&CaseID={case_id}&Version={img_version}"
+            image_set = "R"  # TODO: Get the actual value from the UI.
 
-            request = Request(
-                img_url, headers={"Cookie": cookie}, priority=Priority.IMAGE.value
-            )
-            self.request_handler.enqueue_request(request)
+            img_elements = img_set_lookup.get(image_set, [])
+            for img_element in img_elements:
+                img_id = img_element[0]
+                img_version = img_element[1]
+                img_url = f"https://crashviewer.nhtsa.dot.gov/nass-cds/GetBinary.aspx?Image&ImageID={img_id}&CaseID={case_id}&Version={img_version}"
 
-    def parse_image(self, response_content):
+                request = Request(
+                    img_url, headers={"Cookie": cookie}, priority=Priority.IMAGE.value
+                )
+                self.request_handler.enqueue_request(request)
+                self.case_veh_img_ids[(case_id, vehicle_num)].append(int(img_id))
+
+    def parse_image(self, url, response_content):
+        img_id = int(url.split("&")[1].split("=")[1])
         img = Image.open(BytesIO(response_content))
-        self.img_grid.add_image(img)
-
-
-class ImageViewerWidget(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.images = []
-
-        self.v_layout = QGridLayout()
-        self.scroll_area = QScrollArea()
-        self.scroll_area.setWidgetResizable(True)
-        self.scroll_area.setFixedHeight(200)
-
-        self.thumbnails = QWidget()
-        self.thumbnails_layout = QHBoxLayout()
-        self.thumbnails.setLayout(self.thumbnails_layout)
-
-        self.scroll_area.setWidget(self.thumbnails)
-        self.v_layout.addWidget(self.scroll_area)
-        self.setLayout(self.v_layout)
-
-        self.no_images_label = QLabel("There are no images to display")
-        font = QFont()
-        font.setPointSize(14)
-        self.no_images_label.setFont(font)
-        self.no_images_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.v_layout.addWidget(self.no_images_label, 0, 0)
-        self.no_images_label.setVisible(True)
-
-    def add_image(self, img: Image.Image):
-        self.images.append(img)
+        self.vehicle_imgs[img_id] = img
         self.update_images()
 
     def update_images(self):
-        # Clear existing thumbnails
-        self.no_images_label.setVisible(not len(self.images))
-        for i in reversed(range(self.thumbnails_layout.count())):
-            widget = self.thumbnails_layout.itemAt(i).widget()
-            self.thumbnails_layout.removeWidget(widget)
+        current_idx = self.ui.eventsList.currentIndex()
+        event_data = self.model.data(current_idx, Qt.ItemDataRole.UserRole)
+        img_ids = self.case_veh_img_ids.get((int(event_data["case_id"]), int(event_data["vehicle_num"])), [])
+        images = [self.vehicle_imgs.get(img_id) for img_id in img_ids]
+        images = [img for img in images if img]
+
+        self.no_images_label.setVisible(not len(images))
+        for i in reversed(range(self.ui.thumbnailsLayout.count())):
+            widget = self.ui.thumbnailsLayout.itemAt(i).widget()
+            self.ui.thumbnailsLayout.removeWidget(widget)
             widget.setParent(None)
 
-        # Add new thumbnails to the layout
-        for img in self.images:
+        for img in images:
             thumbnail = QLabel()
             pixmap = self.img_to_pixmap(img).scaledToHeight(150)
             thumbnail.setPixmap(pixmap)
             thumbnail.setAlignment(Qt.AlignmentFlag.AlignVCenter)
             thumbnail.mouseDoubleClickEvent = img.show
-            self.thumbnails_layout.addWidget(thumbnail)
+            self.ui.thumbnailsLayout.addWidget(thumbnail)
 
     def img_to_pixmap(self, img: Image.Image):
         qimg = QImage(
@@ -264,9 +255,3 @@ class ImageViewerWidget(QWidget):
         )
         qimg = QPixmap.fromImage(qimg)
         return qimg
-
-    def clear_images(self):
-        for img in self.images:
-            img.close()
-        self.images = []
-        self.update_images()
