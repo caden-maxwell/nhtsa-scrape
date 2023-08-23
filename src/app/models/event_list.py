9cd87e46e751165908b02a5bbfc7924a747d7009
cmax <a02335780@usu.edu>
2023-08-23 02:08:37 -0600

    Fixed bug with terrible event viewer performance Tabs extend from base tab Moved lots of things around to make more sense

--- src/app/models/event_list.py
+++ src/app/models/event_list.py
@@ -5,7 +5,7 @@ from PyQt6.QtCore import QAbstractListModel, QModelIndex, Qt, QVariant
 from . import DatabaseHandler
 
 
-class ProfileEvents(QAbstractListModel):
+class EventList(QAbstractListModel):
     def __init__(self, db_handler: DatabaseHandler, profile_id):
         super().__init__()
         self.logger = logging.getLogger(__name__)
@@ -18,7 +18,7 @@ class ProfileEvents(QAbstractListModel):
             self.logger.error(f"Profile ID {profile_id} not found.")
             print(f"Profile ID {profile_id} not found.")
             return
-        self.refresh_events()
+        self.refresh_data()
 
     def rowCount(self, parent: QModelIndex = ...) -> int:
         return len(self._data)
@@ -27,7 +27,7 @@ class ProfileEvents(QAbstractListModel):
         if not index.isValid() or not (0 <= index.row() < self.rowCount()):
             return QVariant()
 
-        data = self.all_events()[index.row()]
+        data = self.event_dict(index)
         if role == Qt.ItemDataRole.DisplayRole:
             name = f"{index.row() + 1}. Case: {data['case_id']}, Vehicle: {data['vehicle_num']}, Event: {data['event_num']}"
             return name
@@ -37,41 +37,41 @@ class ProfileEvents(QAbstractListModel):
             return data["ignored"]
 
         return QVariant()
+    
+    def event_dict(self, index: QModelIndex):
+        event = self._data[index.row()]
+        return {
+            "case_id": event[0],
+            "vehicle_num": event[1],
+            "event_num": event[2],
+            "make": event[3],
+            "model": event[4],
+            "model_year": event[5],
+            "curb_weight": event[6],
+            "dmg_loc": event[7],
+            "underride": event[8],
+            "edr": event[9],
+            "total_dv": event[10],
+            "long_dv": event[11],
+            "lat_dv": event[12],
+            "smashl": event[13],
+            "crush": event[14:20],
+            "a_veh_num": event[20],
+            "a_make": event[21],
+            "a_model": event[22],
+            "a_year": event[23],
+            "a_curb_weight": event[24],
+            "a_dmg_loc": event[25],
+            "c_bar": event[26],
+            "NASS_dv": event[27],
+            "NASS_vc": event[28],
+            "e": event[29],
+            "TOT_dv": event[30],
+            "ignored": event[31],
+        }
 
     def all_events(self):
-        """Return events as list of dictionaries"""
-        return [
-            {
-                "case_id": event[0],
-                "vehicle_num": event[1],
-                "event_num": event[2],
-                "make": event[3],
-                "model": event[4],
-                "model_year": event[5],
-                "curb_weight": event[6],
-                "dmg_loc": event[7],
-                "underride": event[8],
-                "edr": event[9],
-                "total_dv": event[10],
-                "long_dv": event[11],
-                "lat_dv": event[12],
-                "smashl": event[13],
-                "crush": event[14:20],
-                "a_veh_num": event[20],
-                "a_make": event[21],
-                "a_model": event[22],
-                "a_year": event[23],
-                "a_curb_weight": event[24],
-                "a_dmg_loc": event[25],
-                "c_bar": event[26],
-                "NASS_dv": event[27],
-                "NASS_vc": event[28],
-                "e": event[29],
-                "TOT_dv": event[30],
-                "ignored": event[31],
-            }
-            for event in self._data
-        ]
+        return [self.event_dict(self.index(i)) for i in range(self.rowCount())]
 
     def index_from_vals(self, case_id, vehicle_num, event_num):
         for i, event in enumerate(self._data):
@@ -83,25 +83,21 @@ class ProfileEvents(QAbstractListModel):
                 return self.index(i)
         return QModelIndex()
 
-    def add_event(self, event):
-        self.db_handler.add_event(event, self.profile[0])
-        self.refresh_events()
-
     def delete_event(self, index: QModelIndex):
         if not index.isValid() or not (0 <= index.row() < self.rowCount()):
             return
         event = self._data[index.row()]
         self.db_handler.delete_event(event, self.profile[0])
-        self.refresh_events()
+        self.refresh_data()
 
     def toggle_ignored(self, index: QModelIndex):
         if not index.isValid() or not (0 <= index.row() < self.rowCount()):
             return
         event = self._data[index.row()]
         self.db_handler.toggle_ignored(event, self.profile[0])
-        self.refresh_events()
+        self.refresh_data()
 
-    def refresh_events(self):
+    def refresh_data(self):
         selected = self.db_handler.get_profile_events(self.profile[0])
         selected = [(t[1], t[2], t[3], t[4]) for t in selected]
         selected.sort()
--- src/app/pages/data_view.py
+++ src/app/pages/data_view.py
@@ -7,25 +7,24 @@ from PyQt6.QtCore import pyqtSignal, pyqtSlot
 from PyQt6.QtWidgets import QWidget
 
 from . import SummaryTab, EventsTab, ScatterTab, CSVTab
-from app.models import DatabaseHandler, ProfileEvents
+from app.models import DatabaseHandler, EventList
 from app.ui.DataView_ui import Ui_DataView
 
 
 class DataView(QWidget):
     exited = pyqtSignal()
 
-    def __init__(self, db_handler: DatabaseHandler, profile_id):
+    def __init__(self, db_handler: DatabaseHandler, profile_id, new_profile=False):
         super().__init__()
 
         self.logger = logging.getLogger(__name__)
-        self.model = ProfileEvents(db_handler, profile_id)
 
         self.ui = Ui_DataView()
         self.ui.setupUi(self)
 
         self.profile = db_handler.get_profile(profile_id)
         # Get a filename-safe string for the new directory
-        profile_name = str(self.profile[1])
+        profile_name = str(self.profile[0])
         # "make": self.ui.makeCombo.currentText().upper(),
         # "model": self.ui.modelCombo.currentText().upper(),
         # "start_year": self.ui.startYearCombo.currentText().upper(),
@@ -48,9 +47,10 @@ class DataView(QWidget):
         dir_name = re.sub(r"[_-]{2,}", "_", dir_name)
         self.data_dir = (Path(__file__).parent.parent / "data" / dir_name).resolve()
 
-        self.summary_tab = SummaryTab()
-        self.events_tab = EventsTab(self.model, self.data_dir)
-        self.scatter_tab = ScatterTab(self.model, self.data_dir)
+        self.summary_tab = SummaryTab(self.profile)
+        model = EventList(db_handler, profile_id)
+        self.events_tab = EventsTab(model, self.data_dir)
+        self.scatter_tab = ScatterTab(model, self.data_dir)
         self.csv_tab = CSVTab(db_handler, profile_id, self.data_dir)
 
         self.ui.tabWidget.addTab(self.summary_tab, "Summary")
@@ -58,12 +58,14 @@ class DataView(QWidget):
         self.ui.tabWidget.addTab(self.scatter_tab, "Scatterplot")
         self.ui.tabWidget.addTab(self.csv_tab, "Data Table")
 
-    @pyqtSlot(dict, bytes, str)
-    def add_event(self, event, response_content, cookie):
-        self.model.add_event(event)
-        self.events_tab.cache_response(int(event["case_id"]), response_content, cookie)
-        self.scatter_tab.update_plot()
-        self.csv_tab.refresh()
+        if new_profile:
+            self.ui.tabWidget.setCurrentWidget(self.events_tab)
+        else:
+            self.ui.tabWidget.setCurrentWidget(self.summary_tab)
+        self.ui.tabWidget.currentChanged.connect(self.update_current_tab)
+
+    def update_current_tab(self):
+        self.ui.tabWidget.currentWidget().refresh_tab()
 
     def closeEvent(self, event):
         self.exited.emit()
