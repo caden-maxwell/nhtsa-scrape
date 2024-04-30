766727da82a1756eaea98fbf58d41044efdb3621
Caden Maxwell <cadenr.maxwell@gmail.com>
2024-04-30 17:58:29 -0600

    removed splashscreen

--- src/pyinstaller.sh
+++ src/pyinstaller.sh
@@ -1,1 +1,1 @@
-pyinstaller --add-data app/resources/settings.json:app/resources --add-data app/resources/payload.json:app/resources -n NHTSA-Scrape --hide-console hide-early --splash app/resources/splash.png main.py
+pyinstaller --add-data app/resources/settings.json:app/resources --add-data app/resources/payload.json:app/resources -n NHTSA-Scrape --hide-console hide-early main.py
