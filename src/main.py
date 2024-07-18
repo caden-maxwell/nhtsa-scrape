45b5198f4e684ff65e211a60222cdd394bea3a68
cmax396 <109488523+caden-maxwell@users.noreply.github.com>
2024-07-18 12:44:13 -0600

    Add app icon

--- src/main.py
+++ src/main.py
@@ -1,12 +1,21 @@
 import sys
+from pathlib import Path
 
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QPixmap, QIcon
 
 from app.main_window import MainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setStyle("fusion")
+
+    # load window icon
+    icon_path = Path(__file__).parent / "app" / "resources" / "icon.png"
+    pixmap = QPixmap()
+    pixmap.loadFromData(icon_path.read_bytes())
+    appIcon = QIcon(pixmap)
+    app.setWindowIcon(appIcon)
 
     main_window = MainWindow()
     main_window.show()
